fe33b49f5a8099d50afae3ca007748671d79948c
Janika <prueljanika000@gmail.com>
2024-01-27 18:04:58 +0200

    update router

--- handlers/router.go
+++ handlers/router.go
@@ -5,4 +5,5 @@ import "net/http"
 func Router() {
 	http.HandleFunc("/", IndexHandler) // Обработчик для корневого пути
 	http.HandleFunc("/ascii-art", AsciiHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 }
