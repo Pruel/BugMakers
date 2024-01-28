e9493ccc9a8634e3f2484298deec409617a52da2
dpruel <dvpruel@gmail.com>
2024-01-28 14:49:46 +0200

    Code review all handlers

--- handlers/ascii_handler.go
+++ handlers/ascii_handler.go
@@ -5,57 +5,58 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
 	ascii "web/internal/ascii_art"
 )
 
-var asciiArtCache string
+var (
+	tmplAsciiArt  *template.Template
+	asciiArtCache string
+)
+
+func init() {
+	var err error
+	tmplAsciiArt, err = template.ParseFiles("./web/templates/asciiArt.html")
+	if err != nil {
+		log.Printf("Error parsing template - ascii_art.html: %v", err)
+	}
+}
 
 func AsciiHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		log.Printf("400")
-		ErrorPage(w, r, "400")
+		log.Println("Method Not Allowed Ascii Handler")
+		ErrorPage(w, r, http.StatusMethodNotAllowed)
 		return
 	}
 
-	// Извлекаем данные из POST запроса
 	text := r.FormValue("text")
 	bannerName := r.FormValue("banner")
-
 	bannerPath := fmt.Sprintf("./internal/banner/%s.txt", bannerName)
 
 	banner, err := ascii.LoadBanner(bannerPath)
 	if err != nil {
 		log.Printf("Failed to load banner: %v", err)
-		ErrorPage(w, r, "500")
+		ErrorPage(w, r, http.StatusInternalServerError)
+		return
+	}
+
+	asciiArt, err := ascii.PrintAscii(banner, text)
+	if err != nil {
+		log.Printf("Failed print ascii: %v", err)
+		ErrorPage(w, r, http.StatusInternalServerError)
 		return
 	}
 
 	log.Printf("Received text: %s", text)
 	log.Printf("Selected banner: %s", bannerName)
-
-	asciiArt, err := ascii.PrintAscii(banner, text)
-	if err != nil {
-		log.Printf("Failed print ascii: %v", err)
-		ErrorPage(w, r, "500")
-		return
-	}
 	log.Printf("Generated ASCII Art:\n%s \n\n\n", asciiArt)
-	asciiArtCache = asciiArt
 
+	asciiArtCache = asciiArt
 	data := map[string]string{
 		"AsciiArt": asciiArtCache,
 	}
 
-	tmpl, err := template.ParseFiles("./web/templates/asciiArt.html")
-	if err != nil {
-		log.Printf("Error parsing template - ascii_art.html: %v", err)
-		ErrorPage(w, r, "404")
-		return
-	}
-
-	if err := tmpl.Execute(w, data); err != nil {
+	if err := tmplAsciiArt.Execute(w, data); err != nil {
 		log.Printf("Error execute template - ascii_art.html: %v", err)
-		ErrorPage(w, r, "500")
+		ErrorPage(w, r, http.StatusInternalServerError)
 	}
 }
